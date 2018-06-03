a1bd2ce28e2791ee0282e90838cd3de9298a6957
robertmartin8 <martin.robertandrew@gmail.com>
2018-06-03 15:36:31 +0800

    updated setup

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
-from distutils.core import setup
+from setuptools import setup
 
-with open("README.rst") as f:
-    readme = f.read()
+desc = """PyPortfolioOpt offers an intuitive, well-documented solution to various portfolio optimisation problems
+that is both extensive and extensible. Currently there is support for maximum-sharpe, minimum-volatility,
+efficient risk (max return for a given risk) and efficient return (min risk for a target return). Classical
+and experimental options are provided, including better estimates of covariance."""
+
 
 setup(
     name="PyPortfolioOpt",
-    version="0.1",
-    description="PyPortfolioOpt: Efficient Frontier, Black Litterman, Monte Carlo optimisation methods",
-    long_description=readme,
+    version="0.1.0",
+    description="PyPortfolioOpt: Efficient Frontier portfolio optimisation",
+    long_description=desc,
+    url="https://github.com/robertmartin8/PyPortfolioOpt",
     author="Robert Andrew Martin",
-    author_email="martin.robertandrew @ gmail.com",
-    packages=["pypfopt", "pypfopt.tests"],
+    author_email="martin.robertandrew@gmail.com",
+    license="MIT",
+    packages=["pypfopt"],
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 3 - Alpha",
         "Environment :: Console",
         "Intended Audience :: Financial and Insurance Industry",
         "Intended Audience :: Science/Research",
@@ -25,4 +30,12 @@ setup(
         "Topic :: Office/Business :: Financial :: Investment",
         "Topic :: Scientific/Engineering :: Mathematics",
     ],
+    keywords="portfolio finance optimization quant trading investing",
+    install_requires=["numpy", "pandas", "pytest", "scikit-learn", "scipy"],
+    project_urls=(
+        {  # TODO add documentation link
+            "Issues?": "https://github.com/robertmartin8/PyPortfolioOpt/issues",
+            "Personal website": "https://reasonabledeviations.science",
+        },
+    ),
 )
