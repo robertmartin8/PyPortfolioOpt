0724d8b069f394832212a7258a5fe6095a3210d0
robertmartin8 <martin.robertandrew@gmail.com>
2018-06-04 11:00:22 +0800

    documentation v0.1.0

--- pypfopt/expected_returns.py
+++ pypfopt/expected_returns.py
@@ -1,23 +1,28 @@
-"""
-This module implements possible models for the expected return.
-It is assumed that daily returns are provided, though in reality the below methods are agnostic
+""" Given historical asset price data, calculate expected returns.
+
+It is assumed that daily returns are provided, though in reality the methods are agnostic
 to the time period (just changed the frequency parameter to annualise).
+
+Currently implemented:
+- mean historical return
+- exponentially weighted mean historical return
 """
+
 import warnings
 import pandas as pd
 
 
 def mean_historical_return(prices, frequency=252):
     """
-    Calculates annualised mean daily historical return from input daily stock prices.
-    If data is not daily, change the frequency.
-    :param prices: daily adjusted closing prices of the asset. Each row is a date
+    Calculate annualised mean daily historical return from input (daily) asset prices.
+
+    :param prices: adjusted closing prices of the asset, each row is a date
                    and each column is a ticker/id.
     :type prices: pd.DataFrame
-    :param frequency: number of days (more generally, number of your desired time period)
-    in a trading year, defaults to 252 days.
+    :param frequency: number of time periods in a year, defaults to 252
+                      (trading days in a year).
     :param frequency: int, optional
-    :return: annualised mean daily return
+    :return: annualised mean daily return for each asset
     :rtype: pd.Series
     """
     if not isinstance(prices, pd.DataFrame):
@@ -31,15 +36,16 @@ def ema_historical_return(prices, frequency=252, span=500):
     """
     Annualised exponentially-weighted mean of daily historical return, giving
     higher weight to more recent data.
-    :param prices: daily adjusted closing prices of the asset. Each row is a date
+
+    :param prices: adjusted closing prices of the asset, each row is a date
                    and each column is a ticker/id.
     :type prices: pd.DataFrame
-    :param frequency: number of days (more generally, number of your desired time period)
-                      in a trading year, defaults to 252 days.
+    :param frequency: number of time periods in a year, defaults to 252
+                      (trading days in a year).
     :param frequency: int, optional
-    :param span: the time period for the EMA, defaults to 500-day EMA.
+    :param span: the time-span for the EMA, defaults to 500-day EMA.
     :type span: int, optional
-    :return: annualised exponentially-weighted mean daily return
+    :return: annualised exponentially-weighted mean daily return of each asset
     :rtype: pd.Series
     """
     if not isinstance(prices, pd.DataFrame):
