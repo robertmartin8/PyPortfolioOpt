073234503dc9458935fc568663914c4c03def6d8
robertmartin8 <martin.robertandrew@gmail.com>
2018-06-04 10:48:48 +0800

    documentation v0.1.0

--- pypfopt/discrete_allocation.py
+++ pypfopt/discrete_allocation.py
@@ -1,12 +1,23 @@
+""" Generate a discrete allocation from continuous weights
+
+get_latest_prices(prices) retrieves the latest asset prices from a dataframe of asset prices.
+
+portfolio(weights, latest_prices, min_allocation, total_portfolio_value) converts continuous weights
+to a discrete allocation that can be purchased.
+"""
+
 import numpy as np
 import pandas as pd
 
 
 def get_latest_prices(prices):
     """
-    Retrieve the latest prices from a dataframe of stock prices.
-    :param prices: daily stock prices
+    This is a helper tool which retrieves the most recent asset prices from a dataframe of
+    asset prices, required in order to generate a discrete allocation.
+
+    :param prices: (daily) stock prices
     :type prices: pd.DataFrame
+    :raises TypeError: if prices are not in a dataframe
     :return: the most recent price of each asset
     :rtype: pd.Series
     """
@@ -17,22 +28,28 @@ def get_latest_prices(prices):
 
 def portfolio(weights, latest_prices, min_allocation=0.01, total_portfolio_value=10000):
     """
-    For a long only portfolio, convert the continuous weights to a discrete allocation.
+    For a long only portfolio, convert the continuous weights to a discrete allocation in a greedy
+    iterative approach. This can be thought of as a clever way to round the continuous weights to an
+    integer number of shares
 
-    :param weights: dictionary of {asset: weight}
+    :param weights: continuous weights generated from the efficient_frontier module
     :type weights: dict
     :param latest_prices: the most recent price for each asset
     :type latest_prices: pd.Series or dict
-    :param min_allocation: any weights less than this number are considered neglibile
+    :param min_allocation: any weights less than this number are considered neglibile,
+                           defaults to 0.01
     :type min_allocation: float, optional
     :param total_portfolio_value: the desired total value of the portfolio, defaults to 10000
-    :type total_portfolio_value: int, optional
+    :type total_portfolio_value: int/float, optional
+    :raises TypeError: if weights isn't a dict
+    :raises TypeError: if latest_prices isn't a series
+    :raises ValueError: if min_allocation is above 0.3 or less than 0
     :return: the number of shares of each ticker that should be purchased, along with the amount
              of funds leftover.
     :rtype: (dict, float)
     """
     if not isinstance(weights, dict):
-        raise TypeError("weights should be a dictionary of {ticker:weight}")
+        raise TypeError("weights should be a dictionary of {ticker: weight}")
     if not isinstance(latest_prices, (pd.Series, dict)):
         raise TypeError("latest_prices should be a pd.Series")
     if min_allocation > 0.3:
@@ -40,17 +57,18 @@ def portfolio(weights, latest_prices, min_allocation=0.01, total_portfolio_value
     if total_portfolio_value <= 0:
         raise ValueError("total_portfolio_value must be greater than zero")
 
-    # Drop any companies with negligible weights
+    # Drop any companies with negligible weights. We use a tuple because order matters.
     nonzero_weights = [(k, v) for k, v in weights.items() if v > min_allocation]
     print(
         f"{len(weights) - len(nonzero_weights)} out of {len(weights)} tickers were removed"
     )
-    # Sort in descending order by weight
+    # Sort in descending order of weight
     nonzero_weights.sort(key=lambda x: x[1], reverse=True)
     available_funds = total_portfolio_value
     shares_bought = []
     buy_prices = []
 
+    # First round
     for ticker, weight in nonzero_weights:
         price = latest_prices[ticker]
         # Attempt to buy the lower integer number of shares
@@ -58,7 +76,7 @@ def portfolio(weights, latest_prices, min_allocation=0.01, total_portfolio_value
         cost = n_shares * price
         if cost > available_funds:
             # Buy as many as possible
-            n_shares = int(available_funds // price)
+            n_shares = available_funds // price
             if n_shares == 0:
                 print("Insufficient funds")
         available_funds -= cost
@@ -67,22 +85,27 @@ def portfolio(weights, latest_prices, min_allocation=0.01, total_portfolio_value
 
     # Second round
     while available_funds > 0:
+        # Calculate the equivalent continuous weights of the shares that
+        # have already been bought
         current_weights = np.array(buy_prices) * np.array(shares_bought)
         current_weights /= current_weights.sum()
         ideal_weights = np.array([i[1] for i in nonzero_weights])
         deficit = ideal_weights - current_weights
 
-        # Find the ticker whose current weights deviate the most
+        # Attempt to buy the asset whose current weights deviate the most
         idx = np.argmax(deficit)
         ticker, weight = nonzero_weights[idx]
         price = latest_prices[ticker]
 
-        counter = 0
-        # If we can't afford this ticker, search for the next highest deficit that we
+        # If we can't afford this asset, search for the next highest deficit that we
         # can purchase.
+        counter = 0
         while price > available_funds:
-            deficit[idx] = 0  # we can no longer purchase the ticker at idx
-            idx = np.argmax(deficit)
+            deficit[idx] = 0  # we can no longer purchase the asset at idx
+            idx = np.argmax(deficit)  # find the next most deviant asset
+
+            # If either of these conditions is met, we break out of both while loops
+            # hence the repeated statement below
             if deficit[idx] < 0 or counter == 10:
                 break
 
@@ -90,17 +113,15 @@ def portfolio(weights, latest_prices, min_allocation=0.01, total_portfolio_value
             price = latest_prices[ticker]
             counter += 1
 
-        # Ugly way to break out, see
-        # https://stackoverflow.com/questions/189645/
         if deficit[idx] <= 0 or counter == 10:
+            # See https://stackoverflow.com/questions/189645/
             break
 
-        # Buy one share
+        # Buy one share at a time
         shares_bought[idx] += 1
         available_funds -= price
 
     print(f"Funds remaining: {available_funds:.2f}")
 
-    # The instance variable is a list of tuples, while the returned value is a dict.
     num_shares = dict(zip([i[0] for i in nonzero_weights], shares_bought))
     return num_shares, available_funds
--- pypfopt/efficient_frontier.py
+++ pypfopt/efficient_frontier.py
@@ -1,3 +1,9 @@
+""" Optimise a portfolio with the Efficient Frontier algorithm.
+
+EfficientFrontier(expected_returns, cov_matrix, weight_bounds) initialises an object which
+will generate an optimal portfolio for various possible objective functions and parameters.
+"""
+
 import warnings
 import numpy as np
 import pandas as pd
@@ -7,37 +13,77 @@ from . import objective_functions
 
 class EfficientFrontier:
 
+    """
+    An EfficientFrontier object contains multiple optimisation methods that can be called
+    (corresponding to different objective functions) with various parameters.
+
+    Instance variables:
+        Inputs:
+            - cov_matrix
+            - n_assets
+            - tickers
+            - bounds
+        Optimisation parameters:
+            - initial_guess
+            - constraints
+            - risk_free_rate
+        Output: weights
+
+    Public methods:
+        - max_sharpe() optimises for maximal Sharpe ratio (a.k.a the tangency portfolio)
+        - min_volatility() optimises for minimum volatility
+        - efficient_risk() maximises Sharpe for a given target risk
+        - efficient_return() minimises risk for a given target return
+        - portfolio_performance() calculates the expected return, volatility and Sharpe ratio for
+        the optimised portfolio.
+    """
+
     def __init__(self, expected_returns, cov_matrix, weight_bounds=(0, 1)):
         """
         :param expected_returns: expected returns for each asset
         :type expected_returns: pd.Series, list, np.ndarray
         :param cov_matrix: covariance of returns for each asset
         :type cov_matrix: pd.DataFrame or np.array
-        :param weight_bounds: minimum and maximum weight of an asset, defaults to (0, 1)
-        :param weight_bounds: tuple, optional
+        :param weight_bounds: minimum and maximum weight of an asset, defaults to (0, 1).
+                              Must be changed to (-1, 1) for portfolios with shorting.
+        :type weight_bounds: tuple, optional
+        :raises TypeError: if expected returns is not a series, list or array
+        :raises TypeError: if cov_matrix is not a dataframe or array
         """
         # Inputs
         if not isinstance(expected_returns, (pd.Series, list, np.ndarray)):
             raise TypeError("Expected returns is not a series, list or array")
-        self.expected_returns = expected_returns
-
         if not isinstance(cov_matrix, (pd.DataFrame, np.ndarray)):
             raise TypeError("cov_matrix is not a dataframe or array")
+
+        self.expected_returns = expected_returns
         self.cov_matrix = cov_matrix
         self.n_assets = len(expected_returns)
         self.tickers = list(expected_returns.index)
+        self.bounds = self._make_valid_bounds(weight_bounds)
+
         # Optimisation parameters
         self.initial_guess = np.array([1 / self.n_assets] * self.n_assets)
         self.constraints = [{"type": "eq", "fun": lambda x: np.sum(x) - 1}]
-        self.bounds = self._make_valid_bounds(weight_bounds)
-        # Optional
         self.risk_free_rate = 0.02
         # Outputs
         self.weights = None
 
     def _make_valid_bounds(self, test_bounds):
-        if len(test_bounds) != 2:
-            raise ValueError("test_bounds must have lower and upper bounds")
+        """
+        Private method: process input bounds into a form acceptable by scipy.optimize,
+        and check the validity of said bounds.
+        :param test_bounds: minimum and maximum weight of an asset
+        :type test_bounds: tuple
+        :raises ValueError: if test_bounds is not a tuple of length two.
+        :raises ValueError: if the lower bound is too high
+        :return: a tuple of bounds, e.g ((0, 1), (0, 1), (0, 1) ...)
+        :rtype: tuple of tuples
+        """
+        if len(test_bounds) != 2 or not isinstance(test_bounds, tuple):
+            raise ValueError(
+                "test_bounds must be a tuple of (lower bound, upper bound)"
+            )
         if test_bounds[0] is not None:
             if test_bounds[0] * self.n_assets > 1:
                 raise ValueError("Lower bound is too high")
@@ -45,13 +91,20 @@ class EfficientFrontier:
 
     def max_sharpe(self, alpha=0, risk_free_rate=0.02):
         """
-        The 'tangent' portfolio that maximises the Sharpe Ratio. The Sharpe ratio is defined as
+        Maximise the Sharpe Ratio, which is defined as:
         .. math::
-        \frac{\mu - R_f}{\sigma}
+            \frac{\mu - R_f}{\sigma}
+        The result is also referred to as the tangency portfolio, as it is the tangent to the
+        efficient frontier curve that intercepts the risk free rate.
+
+        :param alpha: L2 regularisation parameter, defaults to 0. Increase if you want more
+                      non-negligible weights
+        :param alpha: float, optional
         :param risk_free_rate: risk free rate of borrowing/lending, defaults to 0.02
         :type risk_free_rate: float, optional
-        :return: portfolio weights
-        :rtype: dictionary: keys are tickers (string), values are weights (float)
+        :raises ValueError: if alpha or risk_free_rate is non-numeric
+        :return: asset weights for the Sharpe-maximising portfolio
+        :rtype: dict
         """
         if not isinstance(alpha, (int, float)):
             raise ValueError("alpha should be numeric")
@@ -63,7 +116,6 @@ class EfficientFrontier:
         self.risk_free_rate = risk_free_rate
         args = (self.expected_returns, self.cov_matrix, alpha, risk_free_rate)
         constraints = self.constraints
-
         result = sco.minimize(
             objective_functions.negative_sharpe,
             x0=self.initial_guess,
@@ -76,6 +128,16 @@ class EfficientFrontier:
         return dict(zip(self.tickers, self.weights))
 
     def min_volatility(self, alpha=0):
+        """
+        Minimise volatility.
+
+        :param alpha: L2 regularisation parameter, defaults to 0. Increase if you want more
+                      non-negligible weights
+        :param alpha: float, optional
+        :raises ValueError: if alpha or risk_free_rate is non-numeric
+        :return: asset weights for the volatility-minimising portfolio
+        :rtype: dict
+        """
         if not isinstance(alpha, (int, float)):
             raise ValueError("alpha should be numeric")
         if alpha < 0:
@@ -83,7 +145,6 @@ class EfficientFrontier:
 
         args = (self.cov_matrix, alpha)
         constraints = self.constraints
-
         result = sco.minimize(
             objective_functions.volatility,
             x0=self.initial_guess,
@@ -99,12 +160,23 @@ class EfficientFrontier:
         self, target_risk, alpha=0, risk_free_rate=0.02, market_neutral=False
     ):
         """
-        Calculates the Sharpe-maximising portfolio for a given target risk
-        :param self.expected_returns: array of mean returns for a number of stocks
-        :param self.cov_matrix: covariance of these stocks.
-        :param target_risk: the target return
-        :param risk_free_rate: defaults to zero
-        :return: the weights of the portfolio that minimise risk for this target return
+        Calculate the Sharpe-maximising portfolio for a given volatility (i.e max return
+        for a target risk).
+
+        :param target_risk: the desired volatility of the resulting portfolio.
+        :type target_risk: float
+        :param alpha: L2 regularisation parameter, defaults to 0. Increase if you want more
+                      non-negligible weights
+        :param alpha: float, optional
+        :param risk_free_rate: risk free rate of borrowing/lending, defaults to 0.02
+        :type risk_free_rate: float, optional
+        :param market_neutral: whether the portfolio should be market neutral (weights sum to zero),
+                               defaults to False. Requires negative lower weight bound.
+        :param market_neutral: bool, optional
+        :raises ValueError: if target_risk is not a positive float
+        :raises ValueError: if alpha or risk_free_rate is non-numeric
+        :return: asset weights for the efficient risk portfolio
+        :rtype: dict
         """
         if not isinstance(target_risk, float) or target_risk < 0:
             raise ValueError("target_risk should be a positive float")
@@ -117,13 +189,13 @@ class EfficientFrontier:
 
         self.n_assets = len(self.expected_returns)
         args = (self.expected_returns, self.cov_matrix, alpha, risk_free_rate)
-
         target_constraint = {
             "type": "ineq",
             "fun": lambda w: target_risk
             - objective_functions.volatility(w, self.cov_matrix),
         }
-
+        # The equality constraint is either "weights sum to 1" (default), or
+        # "weights sum to 0" (market neutral).
         if market_neutral:
             if self.bounds[0][0] is not None and self.bounds[0][0] >= 0:
                 warnings.warn(
@@ -131,7 +203,6 @@ class EfficientFrontier:
                     RuntimeWarning,
                 )
                 self.bounds = self._make_valid_bounds((-1, 1))
-
             constraints = [
                 {"type": "eq", "fun": lambda x: np.sum(x)},
                 target_constraint,
@@ -152,12 +223,20 @@ class EfficientFrontier:
 
     def efficient_return(self, target_return, alpha=0, market_neutral=False):
         """
-        Calculates the "Markowitz" portfolio, minimising risk for a target return
-        :param self.expected_returns: array of mean returns for a number of stocks
-        :param self.cov_matrix: covariance of these stocks.
-        :param target_risk: the target return
-        :param risk_free_rate: defaults to zero
-        :return: the weights of the portfolio that minimise risk for this target return
+        Calculate the 'Markowitz portfolio', minimising volatility for a given target return.
+
+        :param target_return: the desired return of the resulting portfolio.
+        :type target_return: float
+        :param alpha: L2 regularisation parameter, defaults to 0. Increase if you want more
+                      non-negligible weights
+        :param alpha: float, optional
+        :param market_neutral: whether the portfolio should be market neutral (weights sum to zero),
+                               defaults to False. Requires negative lower weight bound.
+        :param market_neutral: bool, optional
+        :raises ValueError: if target_return is not a positive float
+        :raises ValueError: if alpha is non-numeric
+        :return: asset weights for the Markowitz portfolio
+        :rtype: dict
         """
         if not isinstance(target_return, float) or target_return < 0:
             raise ValueError("target_risk should be a positive float")
@@ -172,6 +251,8 @@ class EfficientFrontier:
             "type": "eq",
             "fun": lambda w: w.dot(self.expected_returns) - target_return,
         }
+        # The equality constraint is either "weights sum to 1" (default), or
+        # "weights sum to 0" (market neutral).
         if market_neutral:
             if self.bounds[0][0] is not None and self.bounds[0][0] >= 0:
                 warnings.warn(
@@ -179,7 +260,6 @@ class EfficientFrontier:
                     RuntimeWarning,
                 )
                 self.bounds = self._make_valid_bounds((-1, 1))
-
             constraints = [
                 {"type": "eq", "fun": lambda x: np.sum(x)},
                 target_constraint,
@@ -200,9 +280,14 @@ class EfficientFrontier:
 
     def portfolio_performance(self, verbose=False):
         """
-        Calculates the performance given the calculated weights of the portfolio
-        :return: [description]
-        :rtype: [type]
+        After optimising, calculate (and optionally print) the performance of the optimal
+        portfolio. Currently calculates expected return, volatility, and the Sharpe ratio.
+
+        :param verbose: whether performance should be printed, defaults to False
+        :param verbose: bool, optional
+        :raises ValueError: if weights have not been calcualted yet
+        :return: expected return, volatility, Sharpe ratio.
+        :rtype: (float, float, float)
         """
         if self.weights is None:
             raise ValueError("Weights not calculated yet")
