dc7e9c89882efd50dbf709f995d47b57f912d8c8
robertmartin8 <martin.robertandrew@gmail.com>
2018-09-11 16:25:24 +0100

    inherit from baseoptimizer

--- pypfopt/efficient_frontier.py
+++ pypfopt/efficient_frontier.py
@@ -8,13 +8,15 @@ import numpy as np
 import pandas as pd
 import scipy.optimize as sco
 from . import objective_functions
+from .base_optimizer import BaseOptimizer
 
 
-class EfficientFrontier:
+class EfficientFrontier(BaseOptimizer):
 
     """
-    An EfficientFrontier object contains multiple optimisation methods that can be called
-    (corresponding to different objective functions) with various parameters.
+    An EfficientFrontier object (inheriting from BaseOptimizer) contains multiple
+    optimisation methods that can be called (corresponding to different objective
+    functions) with various parameters.
 
     Instance variables:
 
@@ -36,6 +38,7 @@ class EfficientFrontier:
 
     - ``max_sharpe()`` optimises for maximal Sharpe ratio (a.k.a the tangency portfolio)
     - ``min_volatility()`` optimises for minimum volatility
+    - ``custom_objective()`` optimises for some custom objective function
     - ``efficient_risk()`` maximises Sharpe for a given target risk
     - ``efficient_return()`` minimises risk for a given target return
     - ``portfolio_performance()`` calculates the expected return, volatility and Sharpe ratio for
@@ -44,7 +47,8 @@ class EfficientFrontier:
 
     def __init__(self, expected_returns, cov_matrix, weight_bounds=(0, 1), gamma=0):
         """
-        :param expected_returns: expected returns for each asset
+        :param expected_returns: expected returns for each asset. Set to None if
+                                 optimising for volatility only.
         :type expected_returns: pd.Series, list, np.ndarray
         :param cov_matrix: covariance of returns for each asset
         :type cov_matrix: pd.DataFrame or np.array
@@ -58,49 +62,30 @@ class EfficientFrontier:
         :raises TypeError: if ``cov_matrix`` is not a dataframe or array
         """
         # Inputs
-        if not isinstance(expected_returns, (pd.Series, list, np.ndarray)):
-            raise TypeError("Expected returns is not a series, list or array")
-        if not isinstance(cov_matrix, (pd.DataFrame, np.ndarray)):
-            raise TypeError("cov_matrix is not a dataframe or array")
-
-        self.expected_returns = expected_returns
         self.cov_matrix = cov_matrix
-        self.n_assets = len(expected_returns)
-        self.tickers = list(expected_returns.index)
-        self.bounds = self._make_valid_bounds(weight_bounds)
+        if expected_returns is not None:
+            if not isinstance(expected_returns, (pd.Series, list, np.ndarray)):
+                raise TypeError(
+                    "expected_returns is not a series, list or array")
+            if not isinstance(cov_matrix, (pd.DataFrame, np.ndarray)):
+                raise TypeError("cov_matrix is not a dataframe or array")
+            self.expected_returns = expected_returns
+            self.tickers = list(expected_returns.index)
+        else:
+            self.tickers = list(cov_matrix.columns)
+        self.n_assets = len(self.tickers)
+
+        super().__init__(self.n_assets, weight_bounds)
 
         if not isinstance(gamma, (int, float)):
             raise ValueError("gamma should be numeric")
         if gamma < 0:
-            warnings.warn("in most cases, gamma should be positive", UserWarning)
+            warnings.warn(
+                "in most cases, gamma should be positive", UserWarning)
         self.gamma = gamma
 
-        # Optimisation parameters
-        self.initial_guess = np.array([1 / self.n_assets] * self.n_assets)
-        self.constraints = [{"type": "eq", "fun": lambda x: np.sum(x) - 1}]
         # Outputs
         self.weights = None
-
-    def _make_valid_bounds(self, test_bounds):
-        """
-        Private method: process input bounds into a form acceptable by scipy.optimize,
-        and check the validity of said bounds.
-
-        :param test_bounds: minimum and maximum weight of an asset
-        :type test_bounds: tuple
-        :raises ValueError: if ``test_bounds`` is not a tuple of length two.
-        :raises ValueError: if the lower bound is too high
-        :return: a tuple of bounds, e.g ((0, 1), (0, 1), (0, 1) ...)
-        :rtype: tuple of tuples
-        """
-        if len(test_bounds) != 2 or not isinstance(test_bounds, tuple):
-            raise ValueError(
-                "test_bounds must be a tuple of (lower bound, upper bound)"
-            )
-        if test_bounds[0] is not None:
-            if test_bounds[0] * self.n_assets > 1:
-                raise ValueError("Lower bound is too high")
-        return (test_bounds,) * self.n_assets
 
     def max_sharpe(self, risk_free_rate=0.02):
         """
@@ -117,15 +102,15 @@ class EfficientFrontier:
         if not isinstance(risk_free_rate, (int, float)):
             raise ValueError("risk_free_rate should be numeric")
 
-        args = (self.expected_returns, self.cov_matrix, self.gamma, risk_free_rate)
-        constraints = self.constraints
+        args = (self.expected_returns, self.cov_matrix,
+                self.gamma, risk_free_rate)
         result = sco.minimize(
             objective_functions.negative_sharpe,
             x0=self.initial_guess,
             args=args,
             method="SLSQP",
             bounds=self.bounds,
-            constraints=constraints,
+            constraints=self.constraints,
         )
         self.weights = result["x"]
         return dict(zip(self.tickers, self.weights))
@@ -134,19 +119,40 @@ class EfficientFrontier:
         """
         Minimise volatility.
 
-        :raises ValueError: if ``risk_free_rate`` is non-numeric
         :return: asset weights for the volatility-minimising portfolio
         :rtype: dict
         """
         args = (self.cov_matrix, self.gamma)
-        constraints = self.constraints
         result = sco.minimize(
             objective_functions.volatility,
             x0=self.initial_guess,
             args=args,
             method="SLSQP",
             bounds=self.bounds,
-            constraints=constraints,
+            constraints=self.constraints,
+        )
+        self.weights = result["x"]
+        return dict(zip(self.tickers, self.weights))
+
+    def custom_objective(self, objective_function):
+        """
+        Optimise some objective function. While an implicit requirement is that the function
+        can be optimised via a quadratic optimiser, this is not enforced. Thus there is a
+        decent chance of silent failure.
+
+        :param objective_function: function which maps (weight, *args) -> cost
+        :type objective_function: function with signature (np.Array, *args) -> float
+        :return: asset weights that optimise the custom objective
+        :rtype: dict
+        """
+        args = (self.cov_matrix, self.gamma)
+        result = sco.minimize(
+            objective_function,
+            x0=self.initial_guess,
+            args=args,
+            method="SLSQP",
+            bounds=self.bounds,
+            constraints=self.constraints,
         )
         self.weights = result["x"]
         return dict(zip(self.tickers, self.weights))
@@ -173,8 +179,8 @@ class EfficientFrontier:
         if not isinstance(risk_free_rate, (int, float)):
             raise ValueError("risk_free_rate should be numeric")
 
-        self.n_assets = len(self.expected_returns)
-        args = (self.expected_returns, self.cov_matrix, self.gamma, risk_free_rate)
+        args = (self.expected_returns, self.cov_matrix,
+                self.gamma, risk_free_rate)
         target_constraint = {
             "type": "ineq",
             "fun": lambda w: target_risk
@@ -223,7 +229,6 @@ class EfficientFrontier:
         if not isinstance(target_return, float) or target_return < 0:
             raise ValueError("target_risk should be a positive float")
 
-        self.n_assets = len(self.expected_returns)
         args = (self.cov_matrix, self.gamma)
         target_constraint = {
             "type": "eq",
@@ -255,27 +260,6 @@ class EfficientFrontier:
         )
         self.weights = result["x"]
         return dict(zip(self.tickers, self.weights))
-
-    def clean_weights(self, cutoff=1e-4, rounding=5):
-        """
-        Helper method to clean the raw weights, setting any weights whose absolute
-        values are below the cutoff to zero, and rounding the rest.
-
-        :param cutoff: the lower bound, defaults to 1e-4
-        :type cutoff: float, optional
-        :param rounding: number of decimal places to round the weights, defaults to 5.
-                         Set to None if rounding is not desired.
-        :type rounding: int, optional
-        :return: asset weights
-        :rtype: dict
-        """
-        if not isinstance(rounding, int) or rounding < 1:
-            raise ValueError("rounding must be a positive integer")
-        clean_weights = self.weights.copy()
-        clean_weights[np.abs(clean_weights) < cutoff] = 0
-        if rounding is not None:
-            clean_weights = np.round(clean_weights, rounding)
-        return dict(zip(self.tickers, clean_weights))
 
     def portfolio_performance(self, verbose=False, risk_free_rate=0.02):
         """
