f36e1e40688fd6fb73b0a936e46383baa0edf3f5
robertmartin8 <martin.robertandrew@gmail.com>
2018-09-23 11:15:46 +0100

    basic HRP test

--- tests/test_hrp.py
+++ tests/test_hrp.py
@@ -5,9 +5,8 @@ from tests.utilities_for_tests import get_data
 
 def test_hrp_portfolio():
     df = get_data()
-    cov = df.pct_change().cov()
-    corr = df.pct_change().corr()
-    w = hrp_portfolio(cov, corr)
+    returns = df.pct_change().dropna(how="all")
+    w = hrp_portfolio(returns)
     assert isinstance(w, dict)
     assert set(w.keys()) == set(df.columns)
     np.testing.assert_almost_equal(sum(w.values()), 1)
