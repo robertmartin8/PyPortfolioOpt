c9cc44a9c3f727131b7a8dc7b048b4fcefa15cb0
robertmartin8 <martin.robertandrew@gmail.com>
2019-07-03 09:11:03 +0800

    finished greedy algo (with shorts)

--- pypfopt/discrete_allocation.py
+++ pypfopt/discrete_allocation.py
@@ -1,11 +1,10 @@
 """
-The ``discrete_allocation`` module contains functions to generate a discrete
-allocation from continuous weights.
+The ``discrete_allocation`` module contains the ``DiscreteAllocation`` class, which
+offers multile methods to generate a discrete portfolio allocation from continuous weights.
 """
-
-import numbers
 import numpy as np
 import pandas as pd
+import pulp
 
 
 def get_latest_prices(prices):
@@ -24,110 +23,226 @@ def get_latest_prices(prices):
     return prices.ffill().iloc[-1]
 
 
-def portfolio(weights, latest_prices, min_allocation=0.01, total_portfolio_value=10000):
+class DiscreteAllocation:
     """
-    For a long only portfolio, convert the continuous weights to a discrete allocation
-    in a greedy iterative approach. This can be thought of as a clever way to round
-    the continuous weights to an integer number of shares
+    Generate a discrete portfolio allocation from continuous weights
 
-    :param weights: continuous weights generated from the ``efficient_frontier`` module
-    :type weights: dict
-    :param latest_prices: the most recent price for each asset
-    :type latest_prices: pd.Series or dict
-    :param min_allocation: any weights less than this number are considered negligible,
-                           defaults to 0.01
-    :type min_allocation: float, optional
-    :param total_portfolio_value: the desired total value of the portfolio, defaults to 10000
-    :type total_portfolio_value: int/float, optional
-    :raises TypeError: if ``weights`` is not a dict
-    :raises TypeError: if ``latest_prices`` isn't a series
-    :raises ValueError: if not ``0 < min_allocation < 0.3``
-    :return: the number of shares of each ticker that should be purchased, along with the amount
-             of funds leftover.
-    :rtype: (dict, float)
+    Instance variables:
+
+    - Inputs:
+
+        - ``weights``
+        - ``latest_prices``
+        - ``min_allocation``
+        - ``total_portfolio_value``
+        - ``short_ratio``
+
+    - Output: ``allocation`` {ticker: number} dict
+
+    Public methods:
+
+    - ``greedy_portfolio()``
+    - ``lp_portfolio()``
     """
-    if not isinstance(weights, dict):
-        raise TypeError("weights should be a dictionary of {ticker: weight}")
-    if not isinstance(latest_prices, (pd.Series, dict)):
-        raise TypeError("latest_prices should be a pd.Series")
-    if min_allocation > 0.3:
-        raise ValueError("min_allocation should be a small float")
-    if total_portfolio_value <= 0:
-        raise ValueError("total_portfolio_value must be greater than zero")
 
-    # Drop any companies with negligible weights. We use a tuple because order matters.
-    nonzero_weights = [(k, v) for k, v in weights.items() if v > min_allocation]
-    print(
-        "{} out of {} tickers were removed".format(
-            len(weights) - len(nonzero_weights), len(weights)
+    def __init__(
+        self,
+        weights,
+        latest_prices,
+        min_allocation=0.01,
+        total_portfolio_value=10000,
+        short_ratio=0.30,
+    ):
+        """
+        :param weights: continuous weights generated from the ``efficient_frontier`` module
+        :type weights: dict
+        :param latest_prices: the most recent price for each asset
+        :type latest_prices: pd.Series or dict
+        :param min_allocation: any weights less than this number are considered negligible,
+                               defaults to 0.01
+        :type min_allocation: float, optional
+        :param total_portfolio_value: the desired total value of the portfolio, defaults to 10000
+        :type total_portfolio_value: int/float, optional
+        :param short_ratio: the short ratio, e.g 0.3 corresponds to 130/30
+        :type short_ratio: float
+        :raises TypeError: if ``weights`` is not a dict
+        :raises TypeError: if ``latest_prices`` isn't a series
+        :raises ValueError: if not ``0 < min_allocation < 0.3``
+        :raises ValueError: if ``short_ratio < 0``
+        """
+        if not isinstance(weights, dict):
+            raise TypeError("weights should be a dictionary of {ticker: weight}")
+        if not isinstance(latest_prices, (pd.Series, dict)):
+            raise TypeError("latest_prices should be a pd.Series")
+        if min_allocation > 0.3:
+            raise ValueError("min_allocation should be a small float")
+        if total_portfolio_value <= 0:
+            raise ValueError("total_portfolio_value must be greater than zero")
+        if short_ratio <= 0:
+            raise ValueError("short_ratio must be positive")
+
+        # Drop any companies with negligible weights. Use a tuple because order matters.
+        self.weights = [
+            (k, v) for k, v in weights.items() if np.abs(v) > min_allocation
+        ]
+        print(
+            "{} out of {} tickers were removed".format(
+                len(weights) - len(self.weights), len(weights)
+            )
         )
-    )
-    # Sort in descending order of weight
-    nonzero_weights.sort(key=lambda x: x[1], reverse=True)
-    available_funds = total_portfolio_value
-    shares_bought = []
-    buy_prices = []
+        self.latest_prices = latest_prices
+        self.min_allocation = min_allocation
+        self.total_portfolio_value = total_portfolio_value
+        self.short_ratio = short_ratio
 
-    # First round
-    for ticker, weight in nonzero_weights:
-        price = latest_prices[ticker]
-        # Attempt to buy the lower integer number of shares
-        n_shares = int(weight * total_portfolio_value / price)
-        cost = n_shares * price
-        if cost > available_funds:
-            # Buy as many as possible
-            n_shares = available_funds // price
-            if n_shares == 0:
-                print("Insufficient funds")
-        available_funds -= cost
-        shares_bought.append(n_shares)
-        buy_prices.append(price)
+    def _allocation_rmse_error(self, verbose=True):
+        """
+        Utility function to calculate and print RMSE error between discretised
+        weights and continuous weights. RMSE was usen instead of MAE because we
+        want to penalise large variations.
 
-    # Second round
-    while available_funds > 0:
-        # Calculate the equivalent continuous weights of the shares that
-        # have already been bought
-        current_weights = np.array(buy_prices) * np.array(shares_bought)
-        current_weights /= current_weights.sum()
-        ideal_weights = np.array([i[1] for i in nonzero_weights])
-        deficit = ideal_weights - current_weights
+        :param verbose: print weight discrepancies?
+        :type verbose: bool
+        :return: rmse error
+        :rtype: float
+        """
+        portfolio_val = 0
+        for ticker, num in self.allocation.items():
+            portfolio_val += num * self.latest_prices[ticker]
 
-        # Attempt to buy the asset whose current weights deviate the most
-        idx = np.argmax(deficit)
-        ticker, weight = nonzero_weights[idx]
-        price = latest_prices[ticker]
+        sse = 0  # sum of square errors
+        for ticker, weight in self.weights:
+            if ticker in self.allocation:
+                allocation_weight = (
+                    self.allocation[ticker] * self.latest_prices[ticker] / portfolio_val
+                )
+            else:
+                allocation_weight = 0
+            sse += (weight - allocation_weight) ** 2
+            if verbose:
+                print(
+                    "{}: allocated {:.3f}, desired {:.3f}".format(
+                        ticker, allocation_weight, weight
+                    )
+                )
+        rmse = np.sqrt(sse / len(self.weights))
+        print("Allocation has RMSE: {:.3f}".format(rmse))
+        return rmse
 
-        # If we can't afford this asset, search for the next highest deficit that we
-        # can purchase.
-        counter = 0
-        while price > available_funds:
-            deficit[idx] = 0  # we can no longer purchase the asset at idx
-            idx = np.argmax(deficit)  # find the next most deviant asset
+    def greedy_portfolio(self):
+        """
+        Convert the continuous weights into a discrete portfolio allocation
+        using a greedy iterative approach.
 
-            # If either of these conditions is met, we break out of both while loops
-            # hence the repeated statement below
-            if deficit[idx] < 0 or counter == 10:
+        :return: the number of shares of each ticker that should be purchased,
+                 along with the amount of funds leftover.
+        :rtype: (dict, float)
+        """
+        # Sort in descending order of weight
+        self.weights.sort(key=lambda x: x[1], reverse=True)
+
+        # TODO figure out shorts
+        # If portfolio contains shorts
+        if self.weights[-1][1] < 0:
+            longs = {t: w for t, w in self.weights if w >= 0}
+            shorts = {t: -w for t, w in self.weights if w < 0}
+
+            # Make them sum to one
+            long_total_weight = sum(longs.values())
+            short_total_weight = sum(shorts.values())
+            longs = {t: w / long_total_weight for t, w in longs.items()}
+            shorts = {t: w / short_total_weight for t, w in shorts.items()}
+
+            # Construct long-only discrete allocations for each
+            short_val = self.total_portfolio_value * self.short_ratio
+
+            print("\nAllocating long sub-portfolio:")
+            da1 = DiscreteAllocation(
+                longs,
+                self.latest_prices[longs.keys()],
+                min_allocation=0,
+                total_portfolio_value=self.total_portfolio_value,
+            )
+            long_alloc, long_leftover = da1.greedy_portfolio()
+
+            print("\nAllocating short sub-portfolio:")
+            da2 = DiscreteAllocation(
+                shorts,
+                self.latest_prices[shorts.keys()],
+                min_allocation=0,
+                total_portfolio_value=short_val,
+            )
+            short_alloc, short_leftover = da2.greedy_portfolio()
+            short_alloc = {t: -w for t, w in short_alloc.items()}
+            # Combine and return
+            return {**long_alloc, **short_alloc}, long_leftover + short_leftover
+
+        # Otherwise, portfolio is long only and we proceed with greedy algo
+        available_funds = self.total_portfolio_value
+        shares_bought = []
+        buy_prices = []
+
+        # First round
+        for ticker, weight in self.weights:
+            price = self.latest_prices[ticker]
+            # Attempt to buy the lower integer number of shares
+            n_shares = int(weight * self.total_portfolio_value / price)
+            cost = n_shares * price
+            if cost > available_funds:
+                # Buy as many as possible
+                n_shares = available_funds // price
+                if n_shares == 0:
+                    print("Insufficient funds")
+            available_funds -= cost
+            shares_bought.append(n_shares)
+            buy_prices.append(price)
+
+        # Second round
+        while available_funds > 0:
+            # Calculate the equivalent continuous weights of the shares that
+            # have already been bought
+            current_weights = np.array(buy_prices) * np.array(shares_bought)
+            current_weights /= current_weights.sum()
+            ideal_weights = np.array([i[1] for i in self.weights])
+            deficit = ideal_weights - current_weights
+
+            # Attempt to buy the asset whose current weights deviate the most
+            idx = np.argmax(deficit)
+            ticker, weight = self.weights[idx]
+            price = self.latest_prices[ticker]
+
+            # If we can't afford this asset, search for the next highest deficit that we
+            # can purchase.
+            counter = 0
+            while price > available_funds:
+                deficit[idx] = 0  # we can no longer purchase the asset at idx
+                idx = np.argmax(deficit)  # find the next most deviant asset
+
+                # If either of these conditions is met, we break out of both while loops
+                # hence the repeated statement below
+                if deficit[idx] < 0 or counter == 10:
+                    break
+
+                ticker, weight = self.weights[idx]
+                price = self.latest_prices[ticker]
+                counter += 1
+
+            if deficit[idx] <= 0 or counter == 10:
+                # See https://stackoverflow.com/questions/189645/
                 break
 
-            ticker, weight = nonzero_weights[idx]
-            price = latest_prices[ticker]
-            counter += 1
+            # Buy one share at a time
+            shares_bought[idx] += 1
+            available_funds -= price
 
-        if deficit[idx] <= 0 or counter == 10:
-            # See https://stackoverflow.com/questions/189645/
-            break
+        print("Funds remaining: {:.2f}".format(available_funds))
 
-        # Buy one share at a time
-        shares_bought[idx] += 1
-        available_funds -= price
-
-    print("Funds remaining: {:.2f}".format(available_funds))
-
-    num_shares = dict(zip([i[0] for i in nonzero_weights], shares_bought))
-    return num_shares, available_funds
+        self.allocation = dict(zip([i[0] for i in self.weights], shares_bought))
+        self._allocation_rmse_error()
+        return self.allocation, available_funds
 
 
-def portfolio_lp(
+def lp_portfolio(
     weights, latest_prices, min_allocation=0.01, total_portfolio_value=10000
 ):
     """
@@ -135,33 +250,10 @@ def portfolio_lp(
     using Mixed Integer Linear Programming. This can be thought of as a clever way to round
     the continuous weights to an integer number of shares
 
-    :param weights: continuous weights generated from the ``efficient_frontier`` module
-    :type weights: dict
-    :param latest_prices: the most recent price for each asset
-    :type latest_prices: pd.Series or dict
-    :param min_allocation: any weights less than this number are considered negligible,
-                           defaults to 0.01
-    :type min_allocation: float, optional
-    :param total_portfolio_value: the desired total value of the portfolio, defaults to 10000
-    :type total_portfolio_value: int/float, optional
-    :raises TypeError: if ``weights`` is not a dict
-    :raises TypeError: if ``latest_prices`` isn't a series
-    :raises ValueError: if not ``0 < min_allocation < 0.3``
     :return: the number of shares of each ticker that should be purchased, along with the amount
-             of funds leftover.
+            of funds leftover.
     :rtype: (dict, float)
     """
-    import pulp
-
-    if not isinstance(weights, dict):
-        raise TypeError("weights should be a dictionary of {ticker: weight}")
-    if not isinstance(latest_prices, (pd.Series, dict)):
-        raise TypeError("latest_prices should be a pd.Series")
-    if min_allocation > 0.3:
-        raise ValueError("min_allocation should be a small float")
-    if total_portfolio_value <= 0:
-        raise ValueError("total_portfolio_value must be greater than zero")
-
     m = pulp.LpProblem("PfAlloc", pulp.LpMinimize)
     vals = {}
     realvals = {}
@@ -169,8 +261,6 @@ def portfolio_lp(
     abss = {}
     remaining = pulp.LpVariable("remaining", 0)
     for k, w in weights.items():
-        if w < min_allocation:
-            continue
         vals[k] = pulp.LpVariable("x_%s" % k, 0, cat="Integer")
         realvals[k] = latest_prices[k] * vals[k]
         etas[k] = w * total_portfolio_value - realvals[k]
@@ -181,70 +271,4 @@ def portfolio_lp(
     m += pulp.lpSum(abss.values()) + remaining
     m.solve()
     results = {k: val.varValue for k, val in vals.items()}
-    return results, remaining.varValue
-
-
-def portfolio_byvalue(
-    weights, steps, min_values, max_values=1e9, total_portfolio_value=10000
-):
-    """
-    For a long only portfolio, convert the continuous weights to a discrete
-    allocation using Mixed Integer Linear Programming. This function assumes
-    that we buy some asset based on value instead of shares, and there is a
-    limit of minimum value and increasing step.
-
-    :param weights: continuous weights generated from the ``efficient_frontier`` module
-    :type weights: dict
-    :param min_values: the minimum value for each asset
-    :type min_values: int/float or dict
-    :param max_values: the maximum value for each asset
-    :type max_values: int/float or dict
-    :param steps: the minimum value increase for each asset
-    :type steps: int/float or dict
-    :param total_portfolio_value: the desired total value of the portfolio, defaults to 10000
-    :type total_portfolio_value: int/float, optional
-    :raises TypeError: if ``weights`` is not a dict
-    :return: the number of value of each ticker that should be purchased,
-             along with the amount of funds leftover.
-    :rtype: (dict, float)
-    """
-    import pulp
-
-    if not isinstance(weights, dict):
-        raise TypeError("weights should be a dictionary of {ticker: weight}")
-    if total_portfolio_value <= 0:
-        raise ValueError("total_portfolio_value must be greater than zero")
-
-    if isinstance(steps, numbers.Real):
-        steps = {k: steps for k in weights}
-    if isinstance(min_values, numbers.Real):
-        min_values = {k: min_values for k in weights}
-    if isinstance(max_values, numbers.Real):
-        max_values = {k: max_values for k in weights}
-
-    m = pulp.LpProblem("PfAlloc", pulp.LpMinimize)
-    vals = {}
-    realvals = {}
-    usevals = {}
-    etas = {}
-    abss = {}
-    remaining = pulp.LpVariable("remaining", 0)
-    for k, w in weights.items():
-        if steps.get(k):
-            vals[k] = pulp.LpVariable("x_%s" % k, 0, cat="Integer")
-            realvals[k] = steps[k] * vals[k]
-            etas[k] = w * total_portfolio_value - realvals[k]
-        else:
-            realvals[k] = vals[k] = pulp.LpVariable("x_%s" % k, 0)
-            etas[k] = w * total_portfolio_value - vals[k]
-        abss[k] = pulp.LpVariable("u_%s" % k, 0)
-        usevals[k] = pulp.LpVariable("b_%s" % k, cat="Binary")
-        m += etas[k] <= abss[k]
-        m += -etas[k] <= abss[k]
-        m += realvals[k] >= usevals[k] * min_values.get(k, steps.get(k, 0))
-        m += realvals[k] <= usevals[k] * max_values.get(k, 1e18)
-    m += remaining == total_portfolio_value - pulp.lpSum(realvals.values())
-    m += pulp.lpSum(abss.values()) + remaining
-    m.solve()
-    results = {k: pulp.value(val) for k, val in realvals.items()}
     return results, remaining.varValue
--- tests/test_discrete_allocation.py
+++ tests/test_discrete_allocation.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import pytest
-from pypfopt import discrete_allocation
+from pypfopt.discrete_allocation import get_latest_prices, DiscreteAllocation
 from pypfopt.efficient_frontier import EfficientFrontier
 from pypfopt.expected_returns import mean_historical_return
 from pypfopt.risk_models import sample_cov
@@ -10,7 +10,7 @@ from tests.utilities_for_tests import get_data
 
 def test_get_latest_prices():
     df = get_data()
-    latest_prices = discrete_allocation.get_latest_prices(df)
+    latest_prices = get_latest_prices(df)
     assert len(latest_prices) == 20
     assert list(latest_prices.index) == list(df.columns)
     assert latest_prices.name == pd.Timestamp(2018, 4, 11)
@@ -19,19 +19,21 @@ def test_get_latest_prices():
 def test_get_latest_prices_error():
     df = get_data()
     with pytest.raises(TypeError):
-        discrete_allocation.get_latest_prices(df.values)
+        get_latest_prices(df.values)
 
 
-def test_portfolio_allocation():
+def test_greedy_portfolio_allocation():
     df = get_data()
     e_ret = mean_historical_return(df)
     cov = sample_cov(df)
     ef = EfficientFrontier(e_ret, cov)
     w = ef.max_sharpe()
 
-    latest_prices = discrete_allocation.get_latest_prices(df)
-    allocation, leftover = discrete_allocation.portfolio(w, latest_prices)
-    assert allocation == {
+    latest_prices = get_latest_prices(df)
+    da = DiscreteAllocation(w, latest_prices)
+    allocation, leftover = da.greedy_portfolio()
+
+    assert da.allocation == {
         "MA": 14,
         "FB": 12,
         "PFE": 51,
@@ -48,22 +50,77 @@ def test_portfolio_allocation():
     np.testing.assert_almost_equal(total + leftover, 10000)
 
 
-def test_portfolio_allocation_errors():
+def test_greedy_portfolio_allocation_short():
+    df = get_data()
+    e_ret = mean_historical_return(df)
+    cov = sample_cov(df)
+    ef = EfficientFrontier(e_ret, cov, weight_bounds=(-1, 1))
+    w = ef.max_sharpe()
+
+    latest_prices = get_latest_prices(df)
+    da = DiscreteAllocation(w, latest_prices)
+    allocation, leftover = da.greedy_portfolio()
+
+    assert da.allocation == {
+        "MA": 15,
+        "PFE": 45,
+        "FB": 8,
+        "BABA": 5,
+        "AAPL": 4,
+        "BBY": 8,
+        "AMZN": 1,
+        "SBUX": 9,
+        "GOOG": 0,
+        "WMT": 2,
+        "XOM": 2,
+        "BAC": -33,
+        "GM": -16,
+        "GE": -43,
+        "SHLD": -114,
+        "AMD": -35,
+        "JPM": -1,
+    }
+    long_total = 0
+    short_total = 0
+    for ticker, num in allocation.items():
+        if num > 0:
+            long_total += num * latest_prices[ticker]
+        else:
+            short_total -= num * latest_prices[ticker]
+    np.testing.assert_almost_equal(long_total + short_total + leftover, 13000)
+
+
+def test_lp_portfolio_allocation_short():
+    pass
+
+
+def test_allocation_rmse_error():
+    df = get_data()
+    e_ret = mean_historical_return(df)
+    cov = sample_cov(df)
+    ef = EfficientFrontier(e_ret, cov, weight_bounds=(-1, 1))
+    w = ef.max_sharpe()
+
+    latest_prices = get_latest_prices(df)
+    da = DiscreteAllocation(w, latest_prices)
+    assert da._allocation_rmse_error()
+
+
+def test_allocation_errors():
     df = get_data()
     e_ret = mean_historical_return(df)
     cov = sample_cov(df)
     ef = EfficientFrontier(e_ret, cov)
     w = ef.max_sharpe()
-    latest_prices = discrete_allocation.get_latest_prices(df)
+    latest_prices = get_latest_prices(df)
 
     with pytest.raises(TypeError):
-        discrete_allocation.portfolio(ef.weights, latest_prices)
-
+        DiscreteAllocation(ef.weights, latest_prices)
     with pytest.raises(TypeError):
-        discrete_allocation.portfolio(w, latest_prices.values.tolist())
-
+        DiscreteAllocation(w, latest_prices.values.tolist())
     with pytest.raises(ValueError):
-        discrete_allocation.portfolio(w, latest_prices, min_allocation=0.5)
-
+        DiscreteAllocation(w, latest_prices, min_allocation=0.5)
     with pytest.raises(ValueError):
-        discrete_allocation.portfolio(w, latest_prices, total_portfolio_value=0)
+        DiscreteAllocation(w, latest_prices, total_portfolio_value=0)
+    with pytest.raises(ValueError):
+        DiscreteAllocation(w, latest_prices, short_ratio=-0.4)
