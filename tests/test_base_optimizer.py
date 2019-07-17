e7f1d40f3336f5a52be4850a312c657f5479153e
Robert Martin <11792905+robertmartin8@users.noreply.github.com>
2019-07-17 08:55:53 +0800

    Fixed failing test for python 3.5
    Apparently in python 3.5 dict values aren't sorted, hence there was an ordering issue. Hence we sort the arrays first.

--- tests/test_base_optimizer.py
+++ tests/test_base_optimizer.py
@@ -92,7 +92,7 @@ def test_clean_weights_no_rounding():
     # in previous commits, this call would raise a ValueError
     cleaned = ef.clean_weights(rounding=None, cutoff=0)
     assert cleaned
-    np.testing.assert_array_almost_equal(ef.weights, list(cleaned.values()))
+    np.testing.assert_array_almost_equal(np.sort(ef.weights), np.sort(list(cleaned.values())))
 
 
 def test_efficient_frontier_init_errors():
