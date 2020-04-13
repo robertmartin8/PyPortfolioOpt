148e308e4c7d75f47744ac1e7acbc22b0d22e2a9
robertmartin8 <martin.robertandrew@gmail.com>
2020-04-13 14:42:58 +0800

    add version to __init__

--- pypfopt/__init__.py
+++ pypfopt/__init__.py
@@ -9,6 +9,8 @@ from .efficient_frontier import EfficientFrontier
 from .hierarchical_portfolio import HRPOpt
 from .risk_models import CovarianceShrinkage
 
+__version__ = "1.0.1"
+
 __all__ = [
     "market_implied_prior_returns",
     "market_implied_risk_aversion",
