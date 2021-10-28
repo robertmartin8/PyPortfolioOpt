059b69302200fcb50aea1a06ef8184d52c846207
EBP Consulting <ebpconsulting@Admins-MacBook-Pro.local>
2021-10-28 14:47:47 +0200

    Add tickers list

---
+++ cookbook/data/Tickers.py
@@ -0,0 +1,32 @@
+class Tickers:
+    OMXC_LARGE_CAP = ["ALK-B.CO","ALMB.CO","AMBU-B.CO","CARL-A.CO","CARL-B.CO","CHR.CO","COLO-B.CO","DANSKE.CO","DEMANT.CO","DFDS.CO","DRLCO.CO","DSV.CO","FLS.CO","G4S.CO","GMAB.CO","GN.CO","ISS.CO","JDAN.CO","JYSK.CO","KBHL.CO","LUN.CO","MAERSK-A.CO","MAERSK-B.CO","NDA-DK.CO","NETC.CO","NOVO-B.CO","NZYM-B.CO","ORSTED.CO","OSSR.CO","PNDORA.CO","RBREW.CO","RILBA.CO","ROCK-A.CO","ROCK-B.CO","SCHO.CO","SIM.CO","SPNO.CO","STG.CO","SYDB.CO","TOP.CO","TRYG.CO","VWS.CO","ZEAL.CO"]
+
+    OMXC_ALL = ["ALK-B.CO", "ALMB.CO", "AMBU-B.CO", "CARL-A.CO", "CARL-B.CO", "CHR.CO", "COLO-B.CO", "DANSKE.CO",
+                    "DEMANT.CO", "DFDS.CO", "DRLCO.CO", "DSV.CO", "FLS.CO", "G4S.CO", "GMAB.CO", "GN.CO", "ISS.CO",
+                    "JDAN.CO", "JYSK.CO", "KBHL.CO", "LUN.CO", "MAERSK-A.CO", "MAERSK-B.CO", "NDA-DK.CO", "NETC.CO",
+                    "NOVO-B.CO", "NZYM-B.CO", "ORSTED.CO", "OSSR.CO", "PNDORA.CO", "RBREW.CO", "RILBA.CO", "ROCK-A.CO",
+                    "ROCK-B.CO", "SCHO.CO", "SIM.CO", "SPNO.CO", "STG.CO", "SYDB.CO", "TOP.CO", "TRYG.CO", "VWS.CO",
+                    "ZEAL.CO", "BAVA.CO", "BNORDIK-CSE.CO", "BO.CO", "BOOZT-DKK.CO", "CBRAIN.CO", "CHEMM.CO",
+                    "COLUM.CO", "DAB.CO", "DNORD.CO", "GABR.CO", "HART.CO", "HH.CO", "HUSCO.CO", "JUTBK.CO", "LASP.CO",
+                    "MATAS.CO", "NKT.CO", "NLFSK.CO", "NNIT.CO", "NTG.CO", "ORPHA.CO", "PAAL-B.CO", "RTX.CO",
+                    "SAS-DKK.CO", "SOLAR-B.CO", "SPG.CO", "SPKSJF.CO", "TCM.CO", "TIV.CO", "TRMD-A.CO", "UIE.CO",
+                    "VJBA.CO", "ZEAL.CO", "AAB.CO", "AGAT.CO", "AGF-B.CO", "AOJ-P.CO", "ATHENA.CO", "ATLA-DKK.CO",
+                    "BIF.CO", "BIOPOR.CO", "BLVIS-A.CO", "BOLIGA.CO", "BORD-B.CO", "CBRAIN.CO", "CEMAT.CO",
+                    "CPHCAP-PREF.CO", "CPHCAP-ST.CO", "DAB.CO", "DANT.CO", "DJUR.CO", "EAC.CO", "FED.CO", "FFARMS.CO",
+                    "FLUG-B.CO", "FYNBK.CO", "GABR.CO", "GERHSP.CO", "GJ.CO", "GREENM.CO", "GRLA.CO", "GYLD-A.CO",
+                    "GYLD-B.CO", "HARB-B.CO", "HVID.CO", "IMAIL.CO", "KLEE-B.CO", "KRE.CO", "LOLB.CO", "LUXOR-B.CO",
+                    "MIGA-B.CO", "MNBA.CO", "MTHH.CO", "NEWCAP.CO", "NORDIC.CO", "NORTHM.CO", "NRDF.CO", "NTR-B.CO",
+                    "ONXEO.CO", "PARKEN.CO", "PRIMOF.CO", "PSNRDC-A.CO", "RBLN-B.CO", "RIAS-B.CO", "ROV.CO", "SANI.CO",
+                    "SBS.CO", "SCD.CO", "SIF.CO", "SIG.CO", "SKAKO.CO", "SKJE.CO", "STRINV.CO", "TCM.CO", "TOTA.CO",
+                    "VIPRO.CO"]
+    NASDAQ_100 = ['ATVI', 'ADBE', 'AMD', 'ALXN', 'ALGN', 'GOOG', 'AMZN', 'AAL', 'AMGN', 'ADI', 'ANSS', 'AAPL',
+                          'AMAT', 'ASML', 'ADSK', 'ADP', 'BIDU', 'BIIB', 'BMRN', 'BKNG', 'AVGO', 'CDNS', 'CDW', 'CERN',
+                          'CHTR', 'CHKP', 'CTAS', 'CSCO', 'CTXS', 'CTSH', 'CMCSA', 'CPRT', 'CSGP', 'COST', 'CSX',
+                          'DLTR', 'EBAY', 'EA', 'EXC', 'EXPE', 'FB', 'FAST', 'FISV', 'FOX', 'FOXA', 'GILD', 'IDXX',
+                          'ILMN', 'INCY', 'INTC',
+                          'INTU', 'ISRG', 'JD', 'KLAC', 'LRCX', 'LBTYA', 'LBTYK', 'LULU', 'MAR', 'MXIM', 'MELI', 'MCHP',
+                          'MU', 'MSFT', 'MDLZ', 'MNST', 'NTAP', 'NTES', 'NFLX', 'NVDA', 'NXPI', 'ORLY', 'PCAR', 'PAYX',
+                          'PYPL', 'PEP', 'QCOM', 'REGN', 'ROST', 'SGEN', 'SIRI', 'SWKS', 'SPLK', 'SBUX', 'SNPS', 'TMUS',
+                          'TTWO', 'TSLA', 'TXN', 'KHC', 'TCOM', 'ULTA', 'UAL', 'VRSN', 'VRSK', 'VRTX', 'WBA', 'WDC',
+                          'WLTW', 'WDAY', 'XEL', 'XLNX']
+    TEST = ["SCHO.CO","SIM.CO","SPNO.CO","STG.CO","SYDB.CO","TOP.CO","TRYG.CO","VWS.CO","ZEAL.CO"] 
